faa84542e0381720f5cd740e774fbdee1ea0ea28
Sanjeev Jha <sanjeevjha882@gmail.com>
2023-11-29 23:59:41 +0530

    Lifecycle method code added

---
+++ lifecycle-methods/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    // Code to run after the component has mounted
+    console.log(`Hi!!`);
+    return () => {
+      // Code to run before the component is unmounted
+    };
+  }, []); // Empty dependency array means it only runs once, similar to componentDidMount
+
+  /* useEffect(() => {
+    // Code to run after every update
+  }); // No dependency array means it runs on every update */
+
+  return <>Hello React!!!</>;
+}
+
+export default App;
